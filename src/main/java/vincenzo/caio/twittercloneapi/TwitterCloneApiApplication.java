602aea0685f9da0c45eb68f47b9fa04dde224a77
Caio Vincenzo <reisdima@hotmail.com>
2024-04-22 10:33:07 -0300

    First commit

---
+++ src/main/java/vincenzo/caio/twittercloneapi/TwitterCloneApiApplication.java
@@ -0,0 +1,13 @@
+package vincenzo.caio.twittercloneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vincenzo/caio/twittercloneapi/TwitterCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vincenzo.caio.twittercloneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
